26ba48fcb315f685ea306c6fd34cd5341f550f3e
Tim Acker <tjacker31@gmail.com>
2022-08-07 20:47:58 -0400

    Adds User class with name and age properties

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hi')
+import { User } from './models/User';
+
+const user = new User({ name: "Tim", age: 39 });
+
+user.set({ name: "Test" });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
