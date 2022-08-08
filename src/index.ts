69eee5671b2c430bbb17828a1507652d56931712
Tim Acker <tjacker31@gmail.com>
2022-08-07 21:15:29 -0400

    Adds an event handler system to User class

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,4 @@ import { User } from './models/User';
 
 const user = new User({ name: "Tim", age: 39 });
 
-user.set({ name: "Test" });
-
-console.log(user.get("name"));
-console.log(user.get("age"));
+console.log(user);
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,15 @@ interface UserProps {
   age?: number;
 }
 
+interface Events {
+  [key: string]: Callback[];
+}
+
+type Callback = () => void;
+
 export class User {
+  private events: Events = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
@@ -12,5 +20,24 @@ export class User {
 
   set(update: UserProps): void {
     this.data = { ...this.data, ...update };
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers) {
+      return;
+    }
+
+    handlers.forEach((callback: Callback) => {
+      callback();
+    });
   }
 }
